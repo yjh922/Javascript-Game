0f577703c0262f53a85bee566824711062b3cbac
yunja <jihyun648@naver.com>
2023-06-15 18:04:41 +0900

    최종

---
+++ GameObject.js
@@ -0,0 +1,33 @@
+class GameObject{
+    constructor(container,src,width,height,x,y,velX,velY){
+        this.container=container;
+        this.img;
+        this.src=src;
+        this.width=width;
+        this.height=height;
+        this.x=x;
+        this.y=y;
+        this.velX=velX;
+        this.velY=velY;
+
+        this.img=document.createElement("img");
+        this.img.src=this.src;
+        this.img.style.width=this.width+"px";
+        this.img.style.height=this.height+"px";
+        this.img.style.position="absolute";
+        this.img.style.left=this.x+"px";
+        this.img.style.top=this.y+"px";
+
+        this.container.appendChild(this.img);
+
+    }
+    tick(){
+        this.x+=this.velX;
+        this.y+=this.velY;
+    }
+    render(){
+        this.img.style.left=this.x+"px";
+        this.img.style.top=this.y+"px";
+
+    }
+}
---
+++ GoalFlag.js
@@ -0,0 +1,3 @@
+class GoalFlag extends GameObject{
+    
+}
